4ee5cf7f5331835330b0e93c6a27085f39e2bc75
Aadhavan <152236707+Aadhavan2906@users.noreply.github.com>
2025-05-30 06:50:41 +0000

    fixed gitignore

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Admin task manager',
+  description: 'Nextjs Task manager',
 }
 
 export default function RootLayout({
